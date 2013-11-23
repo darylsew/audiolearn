9568a9ca22a00de4c415549699a0c527762b85c6
Daryl Sew <darylsew@gmail.com>
2013-11-23 17:27:20 -0500

    remove skf, too much complexity

--- speech/svmdemo.py
+++ speech/svmdemo.py
@@ -1,4 +1,3 @@
-from sklearn.cross_validation import StratifiedKFold
 from sklearn import svm
 from sklearn.metrics import confusion_matrix
 from numpy import array
@@ -6,15 +5,13 @@ from math import sin, cos
 from decimal import Decimal
 from random import random, randint
 
-
 labeled_points = []
 for i in range(0, 1000):
     # generate random point in the domain [-5,5]
     xval = random()*10 - 5
 
-    # randomly choose if it will be sin, cos, or x**2
-    functype = randint(0,2)
-
+    # randomly choose if it will be sin, cos, or x**2 functype = randint(0,2) 
+    functype = randint(0, 2)
     if functype == 0: # cos
         yval = cos(xval)
     elif functype == 1: # sin
@@ -28,23 +25,12 @@ for i in range(0, 1000):
     
 # create anonymized data
 labels = [ p[2] for p in labeled_points ] 
-skf = StratifiedKFold(labels)
 
 unlabeled_points = [ p[:2] for p in labeled_points ]
 unlabeled_points = array(unlabeled_points)
-labels = array(labels)
+labels = array(labels
 
-
-for train, test in skf:
-    clf = svm.SVC()
-    clf.fit(unlabeled_points[train], labels[train])
-    
-
-    predicted = clf.predict(unlabeled_points[test])
-    #print confusion_matrix(labels[test], predicted)
-
-    import pdb; pdb.set_trace()
-    # Type clf.predict([x, y]) to get info on the type of function!
-
-
-
+clf = svm.SVC()
+clf.fit(unlabeled_points, labels)
+import pdb; pdb.set_trace()
+# Type clf.predict([x, y]) to get info on the type of function!
