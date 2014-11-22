5994f5df14285f03059614b6fead7234ea46f613
Daryl Sew <darylsew@gmail.com>
2014-11-22 11:04:41 -0800

    update description

--- unsupervised/cluster.py
+++ unsupervised/cluster.py
@@ -17,6 +17,7 @@ ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
 ms.fit(X)
 labels = ms.labels_
 cluster_centers = ms.cluster_centers_
+#for each x[i], we have a label label[i]
 
 labels_unique = np.unique(labels)
 n_clusters_ = len(labels_unique)
